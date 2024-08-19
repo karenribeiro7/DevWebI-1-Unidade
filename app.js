1febd3c7c68a4b0594c5c2a809519eee1fe0b431
karenribeiro7 <kevr@discente.ifpe.edu.br>
2024-08-19 20:25:51 -0300

    add: função para esconder o menuem telas menores e manter responsividade do site

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-function loadPage(page) {
-    fetch(page)
-        .then(function(response) {
-            if (!response.ok) {
-                throw new Error('A resposta da rede não foi bem-sucedida');
-            }
-            return response.text();
-        })
-        .then(function(data) {
-            document.getElementById('main').innerHTML = data;
-        })
-        .catch(function(error) {
-            console.error('Houve um problema com a operação de carregamento:', error);
-            document.getElementById('main').innerHTML = "<h2>Erro ao carregar a página</h2><p>Desculpe, não foi possível carregar a página.</p>";
-        });
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.querySelector('.navbar__toggle');
+    const navbar = document.querySelector('.navbar');
+
+    toggleButton.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
+});
